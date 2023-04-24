cecde65e5cffffbf4124e3de356f55b2b2092fe0
BerdaliyevB <131638696+BerdaliyevB@users.noreply.github.com>
2023-04-24 12:02:55 +0500

    l

--- backend/modules/production/views/stages/list.php
+++ backend/modules/production/views/stages/list.php
@@ -16,8 +16,8 @@ $this->params['breadcrumbs'][] = $this->title;
 <!-- Page header start -->
 <div class="page-header">
     <ol class="breadcrumb">
-        <li class="breadcrumb-item">Ombor</li>
-        <li class="breadcrumb-item active">Omborlarni boshqarish</li>
+        <li class="breadcrumb-item"><? Yii::t('app', 'Ombor') ?></li>
+        <li class="breadcrumb-item active"><? Yii::t('app', 'Omborlarni boshqarish') ?></li>
     </ol>
 
     <ul class="app-actions">
